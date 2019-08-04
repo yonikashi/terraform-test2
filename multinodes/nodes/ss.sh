5e9b6dbe6b4ee1d4b3ee87959b2bf7a63401251a
test-name <test@email.com>
2019-08-04 13:42:59 +0300

    (fix Sed)

--- multinodes/nodes/ss.sh
+++ multinodes/nodes/ss.sh
@@ -12,10 +12,10 @@ echo -n ", " >> allnodes.txt
 seed=$(cat seeds/core-test-$c | sed '1!d')
 #sed '/^export[[:blank:]]*self_node_name=/ s/$/'"\"core-test-$c\""'/' .setup-env.sh.TMPL >> setup.$c
 #sed '/^export[[:blank:]]*self_node_name=/ s/IIIself_node_nameIII/'"core-test-$c"'/g' setup.$c >> setup-env-a.$c
-sed '/^export[[:blank:]]*self_node_name=/ s/IIIself_node_nameIII/'"core-test-$c"'/g' .setup-env.sh.TMPL >> setup.$c
+sed '/export[[:blank:]]*self_node_name=/ s/IIIself_node_nameIII/'"core-test-$c"'/g' .setup-env.sh.TMPL >> setup.$c
 #sed '/^export[[:blank:]]*self_node_seed=/ s/$/'"\"$seed\""'/' setup.$c >> setup-env-a.$c
 #sed '/^export[[:blank:]]*self_node_seed=/ s/IIIself_node_seedIII/'"$seed"'/g' setup.$c >> setup-env-a.$c
-sed '/^export[[:blank:]]*self_node_seed=/ s/IIIself_node_seedIII/'"$seed"'/g' setup.$c >> setup-env-a.$c
+sed '/export[[:blank:]]*self_node_seed=/ s/IIIself_node_seedIII/'"$seed"'/g' setup.$c >> setup-env-a.$c
 rm setup.$c
 done
 #./stellar-core --genseed | sed -e 's/^Public: //' | sed -e 's/^Secret seed: //' >> core-test-$1
@@ -26,17 +26,17 @@ echo -n "}\"" >> allnodes.txt
 
 seed=$(cat seeds/core-test-$1 | sed '1!d')
 #sed '/^export[[:blank:]]*self_node_name=/ s/$/'"\"core-test-$1\""'/' .setup-env.sh.TMPL >> setup.$1
-sed '/^export[[:blank:]]*self_node_name=/ s/IIIself_node_nameIII/'"core-test-$1"'/g' .setup-env.sh.TMPL >> setup.$1
+sed '/export[[:blank:]]*self_node_name=/ s/IIIself_node_nameIII/'"core-test-$1"'/g' .setup-env.sh.TMPL >> setup.$1
 #sed '/^export[[:blank:]]*self_node_seed=/ s/$/'"\"$seed\""'/' setup.$1 >> setup-env-a.$1
-sed '/^export[[:blank:]]*self_node_seed=/ s/IIIself_node_seedIII/'"$seed"'/g' setup.$1 >> setup-env-a.$1
+sed '/export[[:blank:]]*self_node_seed=/ s/IIIself_node_seedIII/'"$seed"'/g' setup.$1 >> setup-env-a.$1
 rm setup.$1
 ##########################################
 allnodes=$(cat allnodes.txt)
 for (( c=1; c<=$1; c++ ))
 do
 #sed '/^export[[:blank:]]*all_nodes=/ s/$/'"$allnodes"'/'  setup-env-a.$c >> setup-env.$c
-sed '/^export[[:blank:]]*all_nodes=/ s/IIIall_nodesIII/'"$allnodes"'/g' setup-env-a.$c >> setup-env.$c
-#rm setup-env-a.$c
+sed '/export[[:blank:]]*all_nodes=/ s/IIIall_nodesIII/'"$allnodes"'/g' setup-env-a.$c >> setup-env.$c
+rm setup-env-a.$c
 #rm core-test-$c
 done
 rm allnodes.txt
