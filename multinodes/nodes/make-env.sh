66e72d12420dfcb22822ca69c0fd2f5329e89135
test-name <test@email.com>
2019-08-05 06:41:09 +0300

    (fix-prom)

--- multinodes/nodes/make-env.sh
+++ multinodes/nodes/make-env.sh
@@ -26,7 +26,7 @@ sed '/export[[:blank:]]*all_nodes=/ s/IIIall_nodesIII/'"$allnodes"'/g' setup-env
 rm setup-env-a.$c
 done
 
-sed '/export[[:blank:]]*self_node_name=/ s/IIIself_node_nameIII/'"\"core-test-$c\""'/g' .setup-watcher.sh.TMPL >> watcher-setup.1
+sed '/export[[:blank:]]*self_node_name=/ s/IIIself_node_nameIII/'"\"core-watcher-1\""'/g' .setup-watcher.sh.TMPL >> watcher-setup.1
 sed '/export[[:blank:]]*self_node_seed=/ s/IIIself_node_seedIII/'"\"$seed\""'/g' watcher-setup.1 >> setup-env-watcher.1
 sed '/export[[:blank:]]*all_nodes=/ s/IIIall_nodesIII/'"$allnodes"'/g' setup-env-watcher.1 >> setup-env.watcher
 
@@ -42,6 +42,11 @@ sed 's/III/'"$c"'/g' core-ec2-lb-tmpl > ../core-ec2-lb-$c.tf
 done
 
 #Prometheus init
+
+echo "sudo chown  ubuntu:ubuntu /var/run/docker.sock" >> prometheus-env
+echo "sudo chown ubuntu:ubuntu /data/prometheus/make-conf.sh" >> prometheus-env
+echo "sudo chown /data/prometheus/prometheus.yml.TMPL" >> prometheus-env
+echo "sudo chown /data/prometheus/prometheus.yml.TMPL2" >> prometheus-env
 echo  "cp /data/prometheus/prometheus.yml.TMPL2 /data/prometheus/prometheus.yml.TMPL" >> prometheus-env 
 echo  "sudo bash /data/prometheus/make-conf.sh $1 " >> prometheus-env
 
